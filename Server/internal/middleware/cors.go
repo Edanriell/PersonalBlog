29d4b668187ccd4a8daff940ec47afe853aa6a7c
Edanriell <Edanriell@gmail.com>
2025-09-19 22:04:03 +0300

    feature(cors): created cors config

--- Server/internal/middleware/cors.go
+++ Server/internal/middleware/cors.go
@@ -1,1 +1,13 @@
 package middleware
+
+import (
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func CORS() echo.MiddlewareFunc {
+	return middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:3000", "http://localhost:8080"},
+		AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
+	})
+}
