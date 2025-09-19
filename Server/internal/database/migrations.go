c516e555d2e6200c682376be1ba6d8d09f82d60c
Edanriell <Edanriell@gmail.com>
2025-09-19 21:58:50 +0300

    feature(migrations-config): added auto migrations

--- Server/internal/database/migrations.go
+++ Server/internal/database/migrations.go
@@ -1,1 +1,18 @@
 package database
+
+import (
+	"Server/internal/features/articles"
+	"Server/internal/features/newsletter"
+	"Server/internal/features/users"
+
+	"gorm.io/gorm"
+)
+
+func RunMigrations(db *gorm.DB) error {
+	// Run migrations for all models
+	return db.AutoMigrate(
+		&users.User{},
+		&articles.Article{},
+		&newsletter.Subscriber{},
+	)
+}
