ff7575233c0b839bf4c40f7e9e3bcda72de2fe7e
Edanriell <Edanriell@gmail.com>
2025-09-19 20:42:01 +0300

    feature(config): added config

---
+++ Server/internal/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"os"
+)
+
+type Config struct {
+	Port        string
+	DatabaseURL string
+	JWTSecret   string
+	Environment string
+}
+
+func Load() *Config {
+	return &Config{
+		Port:        getEnv("PORT", "8080"),
+		DatabaseURL: getEnv("DATABASE_URL", "postgres://user:password@localhost/blog_db?sslmode=disable"),
+		JWTSecret:   getEnv("JWT_SECRET", "secret-key"),
+		Environment: getEnv("ENVIRONMENT", "development"),
+	}
+}
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
