d53f975cbbe6e5a6e0e8dc47e651943a5cd05a12
Edanriell <Edanriell@gmail.com>
2025-09-20 11:52:49 +0300

    feature(response): added responses, may need fix in future

--- Server/pkg/response/response.go
+++ Server/pkg/response/response.go
@@ -1,1 +1,56 @@
 package response
+
+import (
+	"net/http"
+)
+
+type Response struct {
+	Success bool        `json:"success"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
+	Error   string      `json:"error,omitempty"`
+}
+
+type PaginatedResponse struct {
+	Response
+	Pagination PaginationMeta `json:"pagination"`
+}
+
+type PaginationMeta struct {
+	Page     int   `json:"page"`
+	PageSize int   `json:"page_size"`
+	Total    int64 `json:"total"`
+	Pages    int   `json:"pages"`
+}
+
+func Success(c echo.Context, data interface{}, message string) error {
+	return c.JSON(http.StatusOK, Response{
+		Success: true,
+		Message: message,
+		Data:    data,
+	})
+}
+
+func Error(c echo.Context, statusCode int, message string, err error) error {
+	response := Response{
+		Success: false,
+		Message: message,
+	}
+
+	if err != nil {
+		response.Error = err.Error()
+	}
+
+	return c.JSON(statusCode, response)
+}
+
+func Paginated(c echo.Context, data interface{}, pagination PaginationMeta, message string) error {
+	return c.JSON(http.StatusOK, PaginatedResponse{
+		Response: Response{
+			Success: true,
+			Message: message,
+			Data:    data,
+		},
+		Pagination: pagination,
+	})
+}
