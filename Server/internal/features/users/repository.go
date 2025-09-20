14465a4dafb0f1c3b9256c143975544bfc63bfdd
Edanriell <Edanriell@gmail.com>
2025-09-20 12:33:03 +0300

    feature(users): added repo

--- Server/internal/features/users/repository.go
+++ Server/internal/features/users/repository.go
@@ -1,1 +1,49 @@
 package users
+
+import (
+	"gorm.io/gorm"
+)
+
+type Repository interface {
+	GetByID(id uint) (*User, error)
+	GetByEmail(email string) (*User, error)
+	Create(user *User) error
+	Update(user *User) error
+	Delete(id uint) error
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) Repository {
+	return &repository{db: db}
+}
+
+func (r *repository) GetByID(id uint) (*User, error) {
+	var user User
+	if err := r.db.First(&user, id).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *repository) GetByEmail(email string) (*User, error) {
+	var user User
+	if err := r.db.Where("email = ?", email).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *repository) Create(user *User) error {
+	return r.db.Create(user).Error
+}
+
+func (r *repository) Update(user *User) error {
+	return r.db.Save(user).Error
+}
+
+func (r *repository) Delete(id uint) error {
+	return r.db.Delete(&User{}, id).Error
+}
