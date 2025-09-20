9b99a1b8f72848da202d75ab108b1f3f68a11c7a
Edanriell <Edanriell@gmail.com>
2025-09-20 18:51:44 +0300

    feature(users): added routes, finished users module

--- Server/internal/features/users/routes.go
+++ Server/internal/features/users/routes.go
@@ -1,2 +1,23 @@
 package users
 
+import (
+	"Server/internal/middleware"
+
+	"gorm.io/gorm"
+)
+
+func RegisterRoutes(g *echo.Group, db *gorm.DB) {
+	repo := NewRepository(db)
+	service := NewService(repo)
+	handler := NewHandler(service)
+
+	// Public routes
+	g.POST("/auth/register", handler.Register)
+	g.POST("/auth/login", handler.Login)
+
+	// Protected routes
+	protected := g.Group("/users")
+	protected.Use(middleware.JWTAuth())
+	protected.GET("/:id", handler.GetByID)
+	protected.PUT("/profile", handler.Update)
+}
